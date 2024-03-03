ee433665f44bb79202a17c5d3df317e8bbc2eeaf
FayzaAhmed <79043448+FayzaAhmed@users.noreply.github.com>
2024-03-03 13:48:43 +0200

    the code problems are solved

--- lab4/task.sv
+++ lab4/task.sv
@@ -1,92 +1,100 @@
-// !!! check this code, repeat is not working!
-
-// ! TA mail: ranabadran@aucegypt.edu
 class comm_component;
+    bit [31:0] data;
+    bit [31:0] address;
 
-bit [31:0] data;
-bit [31:0] address;
+    // Constructor to initialize data and address
+    function new();
+        data = 0;
+        address = 0;
+    endfunction
 
-function new();
-	data =0;
-	address =0;
-
-endfunction
-
-function display;
-	$display ("data = %0d , address %0d ", data, address);
-	
-endfunction
-
+    function display;
+        $display("data = %0d, address = %0d", data, address);
+    endfunction
 endclass
-
 
 class transmitter extends comm_component;
+    function new();
+        super.new();
+        data = $urandom;
+        address = $urandom;
+    endfunction
 
-function new();
-	super.new();
-endfunction
-function transmit (mailbox mb_data,mailbox mb_address);
-// 	$randomize;
-	mb_data.try_put($urandom(data));
-	mb_address.try_put($urandom(address));
-endfunction
-
+    function transmit(mailbox mb_data);
+        bit [31:0] tx_data = $urandom;
+        bit [31:0] tx_address = $urandom;
+        mb_data.try_put(tx_data);
+        mb_data.try_put(tx_address);
+        this.data = tx_data;
+        this.address = tx_address;
+    endfunction
 endclass
 
-class receiver extends comm_component;
-function new();
-	super.new();
-endfunction
-function receive (mailbox mb_data,mailbox mb_address);
-  	mb_data.try_get(data);
-	 mb_address.try_get(address);
-endfunction
 
+class receiver extends comm_component;
+    function new();
+        super.new();
+    endfunction
+
+    function receive(mailbox mb_data);
+        bit [31:0] rx_data;
+        bit [31:0] rx_address;
+		
+        // Loop until data is received
+        repeat (1) begin
+            if (mb_data.try_get(rx_data) && mb_data.try_get(rx_address)) begin
+                // Data received successfully
+                this.data = rx_data; // Assign received data to class variable
+                this.address = rx_address; // Assign received address to class variable
+                break;
+            end
+        end
+    endfunction
 endclass
 
 module lab4();
-	mailbox mb_data,mb_address;
+    mailbox mb_data; // Single mailbox for both transmission and reception
 
-//   mb_data.create();
-//   mb_address.create();
-receiver R = new();
-transmitter T = new();
+    // Instantiate receiver and transmitter
+    receiver R = new();
+    transmitter T = new();
 
-initial begin
-    // Create the mailboxes
-    mb_data = new();
-    mb_address = new();
-
-    // Alternatively, you can create the mailboxes using the create method
-    // mb_data.create();
-    // mb_address.create();
-
-    // Loop for 10 iterations
-    for (int i = 0; i < 10; i++) begin
-        // Transmit data
-        T.transmit(mb_data, mb_address);
-        #5; // Wait for transmission to complete
-
-        // Display transmitted data
-        T.display();
-        $display("Packet transmitted");
-
-        // Receive data
-        R.receive(mb_data, mb_address);
-        #5; // Wait for reception to complete
-
-        // Display received data
-        R.display();
-        $display("Packet received");
+    // Initialize mailbox
+    initial begin
+        mb_data = new();
     end
-    $finish; // Terminate simulation after 10 iterations
-// end
 
-end
+    // Task for transmitting packets
+    task transmit();
+    begin
+        for (int i = 0; i < 10; i = i + 1) begin
+            T.transmit(mb_data);
+            #5; // Wait for transmission to complete
+            T.display();
+            $display("Packet transmitted\n");
+        end
+    end
+    endtask
 
-  
-//   $finish;
+    // Task for receiving packets
+    task receive();
+    begin
+        for (int i = 0; i < 10; i = i + 1) begin
+            #10;
+            R.receive(mb_data);
+            R.display();
+            $display("Packet received\n");
+        end
+    end
+    endtask
 
-
+    // Initial block to start transmit and receive tasks
+    initial begin
+        fork
+            transmit();
+            receive();
+        join
+        $finish; // Terminate simulation
+    end
 endmodule
 
